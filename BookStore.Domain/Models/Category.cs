a550309abcb6a153ab3004e7b28150f1fed4e04a
hergele <57711052+bburakseyhan@users.noreply.github.com>
2020-04-08 18:04:56 +0300

    First Domain model added

---
+++ BookStore.Domain/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookStore.Domain.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
