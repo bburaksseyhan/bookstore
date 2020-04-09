bdfaa4ac20b8f72f5542f1ac5365d660ef7b5926
hergele <57711052+bburakseyhan@users.noreply.github.com>
2020-04-09 15:49:21 +0300

    api swagger and controller added

--- BookStore.Api/Controllers/CategoryController.cs
+++ BookStore.Api/Controllers/CategoryController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookStore.Application.Interfaces;
+using BookStore.Application.ViewModel;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -11,5 +13,19 @@ namespace BookStore.Api.Controllers
     [Route("[controller]")]
     public class CategoryController : ControllerBase
     {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] CategoryViewModel categoryViewModel)
+        {
+            _categoryService.Create(categoryViewModel);
+
+            return Ok(categoryViewModel);
+        }
     }
 }
--- BookStore.Api/Startup.cs
+++ BookStore.Api/Startup.cs
@@ -1,15 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using BookStore.Infrastructure.Data.Context;
+using BookStore.Infrastructure.IoC;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace BookStore.Api
 {
@@ -25,6 +22,20 @@ namespace BookStore.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<BookStoreDBContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("BookStoreDb"));
+            });
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo() { Title = "Book Store Api", Version = "v1" });
+            });
+
+            services.AddMediatR(typeof(Startup));
+
+            RegisterServices(services);
+
             services.AddControllers();
         }
 
@@ -46,6 +57,17 @@ namespace BookStore.Api
             {
                 endpoints.MapControllers();
             });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Book Store Api V1");
+            });
+        }
+
+        private static void RegisterServices(IServiceCollection serv)
+        {
+            DependencyContainer.RegisterServices(serv);
         }
     }
 }
