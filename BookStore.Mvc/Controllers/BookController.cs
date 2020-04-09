34a4f418f446d81dcf8d54b8e7277951e169d1a3
hergele <57711052+bburakseyhan@users.noreply.github.com>
2020-04-09 13:08:58 +0300

    Authorize attribute added

--- BookStore.Mvc/Controllers/BookController.cs
+++ BookStore.Mvc/Controllers/BookController.cs
@@ -1,9 +1,11 @@
 ﻿using BookStore.Application.Interfaces;
 using BookStore.Application.ViewModel;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookStore.Mvc.Controllers
 {
+    [Authorize]
     public class BookController : Controller
     {
         private readonly IBookService _bookService;
